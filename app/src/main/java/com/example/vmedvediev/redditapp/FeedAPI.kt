f011f982bf47156b41ae77056e8141a63bd26c28
Volodymyr Medvediev <volodymyr.medvediev@sigma.software>
2018-05-25 18:06:29 +0300

    Added non-static search of posts.

--- app/src/main/java/com/example/vmedvediev/redditapp/FeedAPI.kt
+++ app/src/main/java/com/example/vmedvediev/redditapp/FeedAPI.kt
@@ -3,9 +3,10 @@ package com.example.vmedvediev.redditapp
 import com.example.vmedvediev.redditapp.model.Feed
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface FeedAPI {
 
-    @GET("earthporn/.rss")
-    fun getFeed(): Call<Feed>
+    @GET("{feedName}/.rss")
+    fun getFeed(@Path("feedName") feedName: String): Call<Feed>
 }
